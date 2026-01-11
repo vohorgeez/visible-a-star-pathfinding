52561fbe2ebb5f869fba4bd96fa5e03923b96cc6
vohorgeez <mathieu.cj.vergez@gmail.com>
2026-01-11 20:42:34 +0100

    feat: iterate and display grid cells

---
+++ GridBasics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int width = 3;
+        int height = 3;
+
+        bool[,] blocked = new bool[width, height];
+        blocked[1,2] = true;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                string cellState = blocked[x, y] ? "X" : ".";
+                Console.WriteLine($"Cell ({x},{y}) = {cellState}");
+            }
+        }
+    }
+}
