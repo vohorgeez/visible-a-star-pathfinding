780bd72f0be395eeef0839edcd7c2b6e5152f5b2
Mathieu Vergez <mathieu.cj.vergez@gmail.com>
2026-01-16 20:26:41 +0100

    test: ensure A* returns shortest path on empty grid

--- GridBasics.Tests/UnitTest1.cs
+++ GridBasics.Tests/UnitTest1.cs
@@ -20,4 +20,22 @@ public class AStarPathfinderTests
         Assert.That(path[0], Is.EqualTo((0, 0)));
         Assert.That(path[^1], Is.EqualTo((2, 2)));
     }
+
+    [Test]
+    public void FindPath_EmptyGrid_ReturnsShortestPath()
+    {
+        // Arrange
+        var grid = new Grid(3, 3);
+        var pathfinder = new AStarPathfinder();
+
+        // Act
+        var path = pathfinder.FindPath(grid, (0, 0), (2, 2));
+
+        // Assert
+        Assert.That(path, Is.Not.Null);
+
+        // 4 moves => 5 nodes
+        Assert.That(path!.Count, Is.EqualTo(5),
+            "Path should have minimal length on empty grid.");
+    }
 }
